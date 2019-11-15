e1ed091a1466581f18fcf5628def72fa30082399
onting <hemool123@naver.com>
2019-11-15 21:51:13 +0900

    Define Automata structure

--- _libhanjp/hanjpautomata.c
+++ _libhanjp/hanjpautomata.c
@@ -1,7 +1,19 @@
 #include "hanjpautomata.h"
+#include "hanjpchar.h"
 
-struct HanjpAutoMata{
+//check batchim in state
+enum state{
+    //some states
+};
+
+struct HanjpAutomata{
+    enum state state;
     enum consonant first;
     enum vowel secend;
-    
+    enum consonantState firstState;
+    enum output output_type;
+    gunichar string[4];
 };
+
+static gboolean hanjp_automata_commit(HanjpAutomata* am);
+static gunichar hanjp_automata_flush_internal(HanjpAutomata* am);
