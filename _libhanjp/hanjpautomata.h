4c935af105e7dd44f488a27262dca34420001b7f
onting <hemool123@naver.com>
2019-11-15 21:50:50 +0900

    Define automata prototype

--- _libhanjp/hanjpautomata.h
+++ _libhanjp/hanjpautomata.h
@@ -2,9 +2,24 @@
 #define __HANJP_AUTOMATA_H__
 #include <gmodule.h>
 
+#define AM_RES_ERR -1
+#define AM_RES_NOEVENT 0
+#define AM_RES_COMMIT 1
+
+enum output {
+    HIRAGANA,
+    KATAKANA
+};
+
 typedef struct HanjpAutomata HanjpAutomata;
 
-HanjpAutomata* hanjp_automata_new();
-
+HanjpAutomata* hanjp_automata_new(enum output init_type);
+void hanjp_automata_set_output_type(HanjpAutomata* am, enum output type);
+enum output hanjp_automata_get_output_type(HanjpAutomata* am);
+const gunichar* hanjp_automata_get_string(HanjpAutomata* am);
+gint hanjp_automata_push(gunichar c);
+gunichar hanjp_automata_flush(HanjpAutomata* am);
+void hanjp_automata_reset(HanjpAutomata* am);
+void hanjp_automata_delete(HanjpAutomata* am);
 
 #endif //__HANJP_AUTOMATA_H__
